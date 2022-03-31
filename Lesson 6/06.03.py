a7150c54f4a261b71d04d9992fa0d44530ca94e0
NadiaKorneva <98978606+NadiaKorneva@users.noreply.github.com>
2022-03-31 22:15:21 +0300

    Add files via upload

---
+++ Lesson 6/06.03.py
@@ -0,0 +1,26 @@
+class Worker:
+    name: str
+    surname: str
+    position: str
+    _income: dict
+
+    def __init__(self, name: str, surname: str, position: str, wage: int, bonus: int):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {
+            "wage": wage,
+            "bonus": bonus
+        }
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return sum(self._income.values())
+
+
+Ivan = Position("Ivan", "Simmons", "Accountant", 100000, 5000)
+print(Ivan.get_full_name(), Ivan.get_total_income())
